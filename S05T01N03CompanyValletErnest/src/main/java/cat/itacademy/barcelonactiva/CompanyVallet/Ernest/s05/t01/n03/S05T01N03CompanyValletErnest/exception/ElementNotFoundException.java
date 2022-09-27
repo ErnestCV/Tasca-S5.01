a7bb966d1d07668e6c9738db38e1c7f2dc61de8e
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-09-27 09:45:48 +0200

    Delete S05T01N03CompanyValletErnest directory

--- S05T01N03CompanyValletErnest/src/main/java/cat/itacademy/barcelonactiva/CompanyVallet/Ernest/s05/t01/n03/S05T01N03CompanyValletErnest/exception/ElementNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.barcelonactiva.CompanyVallet.Ernest.s05.t01.n03.S05T01N03CompanyValletErnest.exception;
-
-public class ElementNotFoundException extends RuntimeException{
-    public ElementNotFoundException(String message) {
-        super(message);
-    }
-}
